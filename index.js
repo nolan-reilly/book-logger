7c212714f9c8641e12d1c1eb9de30a5483187087
Nolan Reilly <nolanreilly02@gmail.com>
2024-05-13 12:21:26 -0500

    Add some form code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const form = document.getElementById("form");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,7 +9,28 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById("title");
+    let author = document.getElementById("author");
+    let pages = document.getElementById("pages");
+    let hasRead = document.getElementById("hasRead");
+    
+    if (title.value == "" || author.value == "" || pages == "") {
+        // User didn't enter any values
+        alert("User didn't enter in value fields!");
+        form.style.display = "none";
+    } else {
+        // Create elements on the page from here
+    }
+})
+
+function displayForm() {
+    form.style.display = "block";
+}
+
 function addBookToLibrary() {
-    // Code here...
+   
 }
 
